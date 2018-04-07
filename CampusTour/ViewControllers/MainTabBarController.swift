4bafe073c102030c011194e38cb7c34367e845bd
Annie Cheng <ac962@cornell.edu>
2018-04-06 17:26:53 -0700

    Add map popup view

--- CampusTour/ViewControllers/MainTabBarController.swift
+++ CampusTour/ViewControllers/MainTabBarController.swift
@@ -5,8 +5,10 @@ class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         let searchVC = SearchViewController()
         let poiMapVC = POIMapViewController()
+        let tabBarHeight = self.tabBar.frame.size.height
         searchVC.tabBarItem = UITabBarItem.feedItem
         poiMapVC.tabBarItem = UITabBarItem.poiMapItem
+        poiMapVC.tabBarHeight = tabBarHeight
         setViewControllers([
             UINavigationController(rootViewController: searchVC),
             UINavigationController(rootViewController: poiMapVC),
